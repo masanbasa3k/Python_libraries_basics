4420b46de39f11d15cfbd7b17b86bb5b5affaea8
beyazıt uysal <66223190+masanbasa3k@users.noreply.github.com>
2022-03-21 01:28:56 +0300

    Create randomModule.py

---
+++ randomModule.py
@@ -0,0 +1,42 @@
+import random
+
+#choice
+
+# list1 = [1,2,3,4,5]
+# print(random.choice(list1))
+
+# string1 = "randomModule.py"
+# print(random.choice(string1))
+
+# tuple1 = (1,2,3,4,5)
+# print(random.choice(tuple1))
+
+#randint
+
+# a = random.randint(0, 50)
+# print(a)
+
+# b = random.randint(-50, -5)
+# print(b)
+
+#random
+
+# c = random.random()
+# print(c)
+
+#shuffle
+
+# list1 = [1,2,3,4,5]
+
+# print("listenin kendisi")
+# print(list1)
+
+# random.shuffle(list1)
+# print("ilk karisritma")
+# print(list1)
+
+# random.shuffle(list1)
+# print("ikinci karisritma")
+# print(list1)
+
+#beyazit
